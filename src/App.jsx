c49ac1e8ca08bf122a40412196cef85f11c6135c
ahmed <a.salawu@outlook.com>
2024-04-10 13:52:06 +0000

    Created main section layout and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Layout } from "antd"
-import { Navbar } from './components'
+import { Navbar, Home, Exchanges, CryptoDetails, News, Cryptocurrencies } from './components'
 import './App.css'
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
@@ -10,7 +11,19 @@ function App() {
         <div className="navbar">
           <Navbar />
         </div>
-        <div className="main"></div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/exchanges' element={<Exchanges />} />
+                <Route path='/news' element={<News />} />
+                <Route path='/cryptodetails/:coinId' element={<CryptoDetails />} />
+                <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              </Routes>
+            </div>
+          </Layout>
+        </div>
         <div className="footer"></div>
       </div>
     </>
