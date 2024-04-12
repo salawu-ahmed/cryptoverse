490a03176f84aab16dc2511661d4a519239efc43
ahmed <a.salawu@outlook.com>
2024-04-12 00:43:51 +0000

    created a search filter for cryptocurrencies component

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGetCryptoQuery } from '../services/crytpoApi'
-import { Card, Col, Row } from 'antd'
+import { Card, Col, Input, Row } from 'antd'
 import { Link } from 'react-router-dom'
 import millify from 'millify'
 
@@ -8,10 +8,21 @@ const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100
     const { data: cryptosList, isFetching } = useGetCryptoQuery(count)
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+    const [searchTerm, setSearchTerm] = useState('')
     console.log(cryptos);
-    if(isFetching) return `Loading...`
+    useEffect(() => {
+        const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        setCryptos(filteredData)
+    }, [cryptosList, searchTerm])
+    if (isFetching) return `Loading...`
     return (
         <>
+            {
+            !simplified &&
+                <div className="search-crypto">
+                    <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+                </div>
+            }
             <Row gutter={[32, 32]} className='crypto-card-container'>
                 {cryptos?.map((crypto) => (
                     <Col xs={24} sm={12} lg={6} className='crypto-card' key={crypto.id}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,12 +23,12 @@ const Home = () => {
             </Row>
             <div className="home-heading-container">
                 <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
-                <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show more..</Link></Title>
+                <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show more</Link></Title>
             </div>
             <Cryptocurrencies simplified/>
             <div className="home-heading-container">
                 <Title level={2} className='home-title'>Latest Cryptocurrencies News</Title>
-                <Title level={3} className='show-more'><Link to='/news'>Show more..</Link></Title>
+                <Title level={3} className='show-more'><Link to='/news'>Show more</Link></Title>
             </div>
             <News simplified/>
         </>
